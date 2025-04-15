9d94a95f96859a43d74362eb9214f034f164989b
ulises <kingluslises00@gmail.com>
2025-04-14 23:09:16 -0600

    [Improvement][Ulises] Config project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,55 +1,105 @@
+/*
+ * build.gradle.kts - Module app
+ * Modified by Ulises Gonzalez
+ * Copyright (c) 2025. All rights reserved
+ */
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.com.google.hilt)
+    alias(libs.plugins.com.google.ksp)
 }
 
 android {
-    namespace = "com.travelonboarding.mobile"
-    compileSdk = 35
+    namespace = BuildConfig.APP_NAMESPACE
+    compileSdk = BuildConfig.COMPILE_SDK_VERSION
 
     defaultConfig {
-        applicationId = "com.travelonboarding.mobile"
-        minSdk = 24
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        resValue("string", "APP_NAME", "\"${properties["app.name"]}\"")
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "DATABASE_NAME", "\"${properties["database.name"]}\"")
+
+        applicationId = BuildConfig.APP_ID
+        minSdk = BuildConfig.MIN_SDK_VERSION
+        targetSdk = BuildConfig.TARGET_SDK_VERSION
+        versionCode = ReleaseConfig.VERSION_CODE
+        versionName = ReleaseConfig.VERSION_NAME
+        testInstrumentationRunner = TestBuildConfig.TEST_INSTRUMENTATION_RUNNER
     }
 
     buildTypes {
-        release {
-            isMinifyEnabled = false
+        getByName(BuildTypes.RELEASE) {
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
+            isMinifyEnabled = Build.Release.isMinifyEnabled
+            isShrinkResources = Build.Release.isShrinkResources
+            isDebuggable = Build.Release.enableUnitTestCoverage
+            enableUnitTestCoverage = Build.Release.isDebuggable
+        }
+        getByName(BuildTypes.DEBUG) {
+            isMinifyEnabled = Build.Debug.isMinifyEnabled
+            isDebuggable = Build.Debug.isDebuggable
+            enableUnitTestCoverage = Build.Debug.enableUnitTestCoverage
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
+        buildConfig = true
+        viewBinding = true
+        resValues = true
         compose = true
     }
 }
 
 dependencies {
-
+    // Core
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.bundles.androidx.lifecycle.libs)
+    implementation(libs.bundles.org.coroutines.libs)
+    // Compose
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.animation)
+    implementation(libs.androidx.compose.foundation)
+    // Libs
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.splashscreen)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
+    implementation(libs.androidx.ui.text.google.fonts)
+    // Hilt
+    implementation(libs.bundles.com.google.hilt.libs)
+    implementation(libs.material)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    // Room
+    implementation(libs.bundles.androidx.room.libs)
+    ksp(libs.androidx.room.compiler)
+    // Hilt
+    ksp(libs.com.google.hilt.compiler)
+    // Test
     testImplementation(libs.junit)
+    testImplementation(libs.com.google.hilt.testing)
+    testImplementation(libs.bundles.org.mockito.libs)
+    testImplementation(libs.org.robolectric)
+    testImplementation(libs.org.coroutines.test)
+    // AndroidTest
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+/*
+ * build.gradle.kts - Project app
+ * Modified by Ulises Gonzalez
+ * Copyright (c) 2025. All rights reserved
+ */
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.com.google.hilt) apply false
+    alias(libs.plugins.com.google.ksp) apply false
 }
